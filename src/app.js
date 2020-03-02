e001a68002ecc0f45bb8cfddc0924cdba06cb2e3
Patrick Gustafson <patrickgus@charter.net>
2020-03-02 15:57:06 -0800

    state client origin in cors

--- src/app.js
+++ src/app.js
@@ -3,29 +3,25 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
-const { NODE_ENV } = require("./config");
+const { NODE_ENV, CLIENT_ORIGIN } = require("./config");
+const errorHandler = require("./error-handler");
 
 const app = express();
 
 const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
-app.use(morgan(morganOption));
+app.use(
+  morgan(morganOption, {
+    skip: () => NODE_ENV === "test"
+  })
+);
 app.use(helmet());
-app.use(cors());
+app.use(cors({ origin: CLIENT_ORIGIN }));
 
 app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
 
-app.use((error, req, res, next) => {
-  let response;
-  if (NODE_ENV === "production") {
-    response = { error: { message: "server error" } };
-  } else {
-    console.error(error);
-    response = { message: error.message, error };
-  }
-  res.status(500).json(response);
-});
+app.use(errorHandler);
 
 module.exports = app;

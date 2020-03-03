7170b138f835fe5036be8c698e2c1c69f6d2bd4b
Patrick Gustafson <patrickgus@charter.net>
2020-03-03 12:17:49 -0800

    format code

--- src/config.js
+++ src/config.js
@@ -2,7 +2,9 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || "http://localhost:3000/",
-  DATABASE_URL: process.env.DATABASE_URL || "postgresql://patrick_gus@localhost/stsaver",
+  DATABASE_URL:
+    process.env.DATABASE_URL || "postgresql://patrick_gus@localhost/stsaver",
   TEST_DATABASE_URL:
-    process.env.TEST_DATABASE_URL || "postgresql://patrick_gus@localhost/stsaver_test"
+    process.env.TEST_DATABASE_URL ||
+    "postgresql://patrick_gus@localhost/stsaver_test"
 };

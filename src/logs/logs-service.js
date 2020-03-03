debc8c475c12165d9a09f66033a3b5631cd5f21d
Patrick Gustafson <patrickgus@charter.net>
2020-03-03 14:04:01 -0800

    add getLogsByUserId method

--- src/logs/logs-service.js
+++ src/logs/logs-service.js
@@ -1,6 +1,20 @@
 const LogsService = {
-  getAllLogs(db) {
-    return db.from("stsaver_logs").select("*");
+  getLogsByUserId(db, user_id) {
+    return db
+      .from("stsaver_logs AS log")
+      .select(
+        "log.id",
+        "log.start_time",
+        "log.end_time",
+        "log.media",
+        "log.breaks",
+        "log.date_added",
+        "log.user_id",
+        db.raw(`(DATE_PART('day',  log.end_time::timestamp - log.start_time::timestamp) * 24 + 
+          DATE_PART('hour', log.end_time::timestamp - log.start_time::timestamp)) AS hours`)
+      )
+      .leftJoin("stsaver_users AS user", "log.user_id", "user.id")
+      .where("log.user_id", user_id);
   }
 };
 

f4fd071be3861dba6fc3654d080f4c7c84287c6a
Patrick Gustafson <patrickgus@charter.net>
2020-03-03 14:53:59 -0800

    add endpoint for posting a log

--- src/logs/logs-router.js
+++ src/logs/logs-router.js
@@ -1,14 +1,37 @@
+const path = require("path");
 const express = require("express");
 const LogsService = require("./logs-service");
 
 const logsRouter = express.Router();
+const jsonParser = express.json();
 
-logsRouter.route("/:user_id/").get((req, res, next) => {
-  LogsService.getLogsByUserId(req.app.get("db"), req.params.user_id)
-    .then(logs => {
-      res.json(logs);
-    })
-    .catch(next);
-});
+logsRouter
+  .route("/:user_id/")
+  .get((req, res, next) => {
+    LogsService.getLogsByUserId(req.app.get("db"), req.params.user_id)
+      .then(logs => {
+        res.json(logs);
+      })
+      .catch(next);
+  })
+  .post(jsonParser, (req, res, next) => {
+    const { start_time, end_time, media, breaks,user_id } = req.body;
+    const newLog = { start_time, end_time, media, breaks, user_id };
+
+    for (const [key, value] of Object.entries(newLog))
+      if (value == null)
+        return res.status(400).json({
+          error: { message: `Missing '${key}' in request body` }
+        });
+
+    LogsService.insertLog(req.app.get("db"), newLog)
+      .then(log => {
+        res
+          .status(201)
+          .location(path.posix.join(req.originalUrl, `/${log.id}`))
+          .json(log);
+      })
+      .catch(next);
+  });
 
 module.exports = logsRouter;

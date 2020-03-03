5ef6d761b0eb48663f264af74ef883c28d34f929
Patrick Gustafson <patrickgus@charter.net>
2020-03-02 16:11:35 -0800

    add db urls

--- src/config.js
+++ src/config.js
@@ -1,5 +1,8 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || "http://localhost:3000/"
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || "http://localhost:3000/",
+  DB_URL: process.env.DB_URL || "postgresql://patrick_gus@localhost/stsaver",
+  TEST_DB_URL:
+    process.env.TEST_DB_URL || "postgresql://patrick_gus@localhost/stsaver_test"
 };

0f1deb93c9da49b75a7c5738f31137a3147ab9b2
Patrick Gustafson <patrickgus@charter.net>
2020-03-04 10:05:47 +0000

    import auth router

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const helmet = require("helmet");
 const { NODE_ENV, CLIENT_ORIGIN } = require("./config");
 const logsRouter = require("./logs/logs-router");
+const authRouter = require("./auth/auth-router");
 const usersRouter = require("./users/users-router");
 const errorHandler = require("./error-handler");
 
@@ -21,6 +22,7 @@ app.use(helmet());
 app.use(cors({ origin: CLIENT_ORIGIN }));
 
 app.use("/api/logs", logsRouter);
+app.use("/api/auth", authRouter);
 app.use("/api/users", usersRouter);
 
 app.use(errorHandler);

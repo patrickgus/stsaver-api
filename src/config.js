5ce0eb09c6ed53cc0600847085539444d7a2465e
Patrick Gustafson <patrickgus@charter.net>
2020-03-04 10:14:27 +0000

    add jwt env vars

--- src/config.js
+++ src/config.js
@@ -6,5 +6,7 @@ module.exports = {
     process.env.DATABASE_URL || "postgresql://patrick_gus@localhost/stsaver",
   TEST_DATABASE_URL:
     process.env.TEST_DATABASE_URL ||
-    "postgresql://patrick_gus@localhost/stsaver_test"
+    "postgresql://patrick_gus@localhost/stsaver_test",
+  JWT_SECRET: process.env.JWT_SECRET || "change-this-secret",
+  JWT_EXPIRY: process.env.JWT_EXPIRY || "3h"
 };

c4366854f2ce8d3a38f576773eec80f92755d360
Patrick Gustafson <patrickgus@charter.net>
2020-03-11 18:35:32 -0700

    change database url

--- src/config.js
+++ src/config.js
@@ -3,7 +3,8 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || "development",
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || "https://stsaver-app.now.sh",
   DATABASE_URL:
-    process.env.DATABASE_URL || "postgresql://patrick_gus@localhost/stsaver",
+    process.env.DATABASE_URL ||
+    "postgres://hcpyicqpvkioye:f5f8f6b3220ecc955cf26e7ed55899249d23f0d18f3daa77ecd59db88b7eb5d5@ec2-35-168-54-239.compute-1.amazonaws.com:5432/d3808r8862mrph",
   TEST_DATABASE_URL:
     process.env.TEST_DATABASE_URL ||
     "postgresql://patrick_gus@localhost/stsaver_test",

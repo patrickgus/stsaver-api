e5f16821d87054beb2b0b3901c27edda81b60090
Patrick Gustafson <patrickgus@charter.net>
2020-03-13 14:55:00 -0700

    change logs table data type

--- migrations/001.do.create_stsaver_logs.sql
+++ migrations/001.do.create_stsaver_logs.sql
@@ -1,7 +1,7 @@
 CREATE TABLE stsaver_logs (
   id SERIAL PRIMARY KEY,
-  start_time TIMESTAMPTZ NOT NULL,
-  end_time TIMESTAMPTZ NOT NULL,
+  start_time TIMESTAMP NOT NULL,
+  end_time TIMESTAMP NOT NULL,
   breaks INTEGER DEFAULT 0 NOT NULL,
   date_added TIMESTAMP DEFAULT now() NOT NULL
 );
